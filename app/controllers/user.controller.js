fe291c224314241a5230e296a1af4d89017f9c9a
Nicolas Galliere <ngal33@hotmail.fr>
2023-01-05 17:40:12 +0100

    main: routes/controller - ajout des fichiers (vides) concernant les ingredients et les recettes ainsi que la fonctionnalité de patch des users (uniquement le mot de passe)

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,3 +1,63 @@
+const db = require("../models");
+const userRepository = db.user;
+const Op = db.Sequelize.Op;
+const {authJwt} = require("../middleware");
+const bcrypt = require("bcryptjs");
+
+// Changement des informations utilisateur
+exports.patchUser = async (req, res) => {
+    // Organisation du body
+    const password = {
+        oldPassword: req.body.oldPassword,
+        newPassword: req.body.newPassword
+    }
+
+    // Récupération du user à modifier
+    const userToModify = await userRepository.findByPk(req.params.id);
+
+    // Comparaison des mots de passe criptés
+    let passwordIsValid = bcrypt.compareSync(
+        password.oldPassword,
+        userToModify.password
+    );
+
+    // Vérification qu'un user modifie ses propres informations
+    if (req.userId == req.params.id) {
+        // Validation de l'exhaustivité des informations à modifer
+        if (!password.newPassword || !password.oldPassword) {
+            res.status(400).send({
+                message: "Il manque l'un des mots de passe dans la requête."
+            });
+        }
+
+        // Vérification de l'ancien mot de passe
+        else if (!passwordIsValid) {
+            res.status(400).send({
+                message: "L'ancien mot de passe est erroné."
+            });
+        }
+
+        // Modification du mot de passe si tout est ok
+        else {
+            db.user.password = bcrypt.hashSync(req.body.newPassword, 8);
+            res.status(200).send({
+                message: "Le mot de passe a été changé."
+            });
+        }
+    }
+
+    // message d'erreur lorsqu'un user souhaite modifier les informations d'un autre user
+    else {
+        res.status(403).send({
+            message: "Vous n'avez pas les droits nécessaires à cette action."
+        })
+    }
+};
+
+
+
+
+
 exports.allAccess = (req, res) => {
     res.status(200).send("Public Content.");
 };
