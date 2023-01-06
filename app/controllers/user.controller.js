69383829b6ad0b6851f28a5a7b354840d8d21cb2
Nicolas Galliere <ngal33@hotmail.fr>
2023-01-06 10:21:18 +0100

    main: user.controller - fonction patch des users complète

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -6,42 +6,54 @@ const bcrypt = require("bcryptjs");
 
 // Changement des informations utilisateur
 exports.patchUser = async (req, res) => {
-    // Organisation du body
-    const password = {
-        oldPassword: req.body.oldPassword,
-        newPassword: req.body.newPassword
-    }
-
     // Récupération du user à modifier
     const userToModify = await userRepository.findByPk(req.params.id);
 
+    // Organisation du body
+    const credentials = {
+        oldPassword: req.body.oldPassword,
+        newPassword: req.body.newPassword || req.body.oldPassword,
+        newEmail: req.body.newEmail || userToModify.email
+    }
+
     // Comparaison des mots de passe criptés
     let passwordIsValid = bcrypt.compareSync(
-        password.oldPassword,
+        credentials.oldPassword,
         userToModify.password
     );
 
     // Vérification qu'un user modifie ses propres informations
     if (req.userId == req.params.id) {
         // Validation de l'exhaustivité des informations à modifer
-        if (!password.newPassword || !password.oldPassword) {
+        if (!credentials.oldPassword) {
             res.status(400).send({
-                message: "Il manque l'un des mots de passe dans la requête."
+                message: "Il manque le mot de passe actuel pour valider la modification."
             });
         }
 
         // Vérification de l'ancien mot de passe
         else if (!passwordIsValid) {
             res.status(400).send({
-                message: "L'ancien mot de passe est erroné."
+                message: "Le mot de passe actuel est erroné."
             });
         }
 
-        // Modification du mot de passe si tout est ok
+        // Modification des informations si tout est ok
         else {
-            db.user.password = bcrypt.hashSync(req.body.newPassword, 8);
+            // Utilisation de .update avec un body déclaré à la main
+            await userRepository.update({
+                username: userToModify.username,
+                email: credentials.newEmail,
+                password: bcrypt.hashSync(credentials.newPassword, 8)
+            },
+                {
+                    where : {id: req.params.id}
+                }).catch(err => {
+                res.status(500).send({
+                    message: "Erreur dans le changement d'identifiants pour l'id=" + req.params.id
+                })});
             res.status(200).send({
-                message: "Le mot de passe a été changé."
+                message: "Les informations de l'utilisateur ont été changées."
             });
         }
     }
